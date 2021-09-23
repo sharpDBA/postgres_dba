9e61f611d5c8c0b2e28d61d2119844d45598df37
Jessica Sharp <31940586+sharpDBA@users.noreply.github.com>
2021-09-22 23:20:37 -0600

    Update locks_andor_blocks.sh

--- locks_andor_blocks/locks_andor_blocks.sh
+++ locks_andor_blocks/locks_andor_blocks.sh
@@ -5,4 +5,4 @@ database=$2
 db_port="5432"
 db_user=$4
 
-psql -h $1 -d $2 -p $db_port -f locks_andor_blocks.sql >> locks_blocks_`date +%d%b%Y`.out
+psql -h $1 -d $2 -p $db_port -f locks_andor_blocks.sql >> locks_blocks_`date +%d%b%Y_%H_%M`.out
